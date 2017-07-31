93e64fb98a8dbc742c5f3b0ca156b3f3b8869529
BraxtonHath <Braxtonhatha@yahoo.com>
2017-07-31 10:31:17 -0400

    step 2

--- src/main/java/io/braxton/topicvoter/controller/TopicController.java
+++ src/main/java/io/braxton/topicvoter/controller/TopicController.java
@@ -1,12 +1,25 @@
 package io.braxton.topicvoter.controller;
 
+import io.braxton.topicvoter.models.Topic;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class TopicController {
     @RequestMapping("/")
-    public String index(){
+    public String index(Model model){ //add <---model model alt enter and stuff
+        Topic first = new Topic(1, "Java Spring", "Not sure how Spring works");
+        Topic second = new Topic(2, "Java Sprin", "Not sure how Spring wor");
+        Topic third = new Topic(3, "Java Spri", "Not sure how Spring wor");
+        ArrayList<Topic> topics = new ArrayList<>();
+        topics.add(first);
+        topics.add(second);
+        topics.add(third);
+        model.addAttribute("topics", topics);
+
         return "index";
     }
 }
--- src/main/java/io/braxton/topicvoter/models/Topic.java
+++ src/main/java/io/braxton/topicvoter/models/Topic.java
@@ -1,4 +1,37 @@
 package io.braxton.topicvoter.models;
 
 public class Topic {
+    private long id;
+    private String title;
+    private String description;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Topic(long id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
 }
